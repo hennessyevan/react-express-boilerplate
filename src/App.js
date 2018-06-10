7120b282def33a8a798cf7e14207cf9ef81da650
Evan Hennessy <hennessyevan@Evans-MacBook-Pro.local>
2018-06-10 13:11:16 -0400

    Init

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class App extends Component {
+	async componentDidMount() {
+		try {
+			const response = await axios.get("/healthcheck");
+			console.log(response.data);
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	render() {
+		return <div>Hello World.</div>;
+	}
+}
+
+export default App;
