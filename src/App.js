a7dbb959e6c7e5cc067444dd900a3c17d1250333
Evan Hennessy <hennessyevan@users.noreply.github.com>
2018-06-15 23:18:52 -0400

    Frontend Requests

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import axios from "axios";
 
 class App extends Component {
+	state = {
+		healthcheck: "",
+		login: {}
+	};
 	async componentDidMount() {
 		try {
-			const response = await axios.get("/healthcheck");
-			console.log(response.data);
+			const healthcheck = await axios.get("/healthcheck");
+			this.setState({ healthcheck: healthcheck.data });
+		} catch (error) {
+			console.log(error);
+		}
+
+		try {
+			const login = await axios.post("/login");
+			this.setState({ login: login.data });
 		} catch (error) {
 			console.log(error);
 		}
 	}
 
 	render() {
-		return <div>Hello World.</div>;
+		return (
+			<Fragment>
+				<pre>Get Healthcheck: {this.state.healthcheck}</pre>
+				<pre>Login response: {JSON.stringify(this.state.login)}</pre>
+			</Fragment>
+		);
 	}
 }
 
